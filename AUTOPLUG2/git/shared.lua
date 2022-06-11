ba9dd9febfa1d9f6eebdf735aa3262e2fe4be274
Kaansi Bewa <81228260+hedhehd@users.noreply.github.com>
2022-06-11 10:44:29 +0200

    Update shared.lua

--- AUTOPLUG2/git/shared.lua
+++ AUTOPLUG2/git/shared.lua
@@ -1,7 +1,12 @@
 -- Constants
-APP_REPO = "ONElua"
-APP_PROJECT = "AutoPlugin2"--"TEST"--
+APP_REPO = "hedhehd"
+APP_PROJECT = "AutoPlugin2-Remaster"--"TEST"--
 APP_FOLDER = "AUTOPLUG2"
+
+-- Constants 2
+APP_REPO2 = "ONELua"
+APP_PROJECT2 = "AutoPlugin2"--"TEST"--
+APP_FOLDER2 = "AUTOPLUG2"
 
 APP_VERSION_MAJOR = 0x01 -- major.minor
 APP_VERSION_MINOR = 0x25
