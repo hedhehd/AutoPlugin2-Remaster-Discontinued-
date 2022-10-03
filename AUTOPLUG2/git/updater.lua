c5580b05ebd77c737f863d1d23fea3c7610c6a71
Kaansi Bewa <81228260+hedhehd@users.noreply.github.com>
2022-10-03 16:05:13 +0200

    Add files via upload

--- AUTOPLUG2/git/updater.lua
+++ AUTOPLUG2/git/updater.lua
@@ -29,6 +29,7 @@ function screen.flip()
 
 			local url = "https://hedhehd.github.io/Downloads-For-My-Repos/AutoPlugin2-Remaster.vpk"
 			local path2vpk = "ux0:data/"..APP_PROJECT..".vpk"
+			files.delete(path2vpk)
 			local onAppInstallOld = onAppInstall
 			function onAppInstall(step, size_argv, written, file, totalsize, totalwritten)
 				return 10 -- Ok code
@@ -50,11 +51,17 @@ function screen.flip()
 				screen.flip()
 
 				buttons.read()
-				--if buttons.cancel then return 0 end --Cancel or Abort
-				return 1;
+				--[[
+				if buttons.cancel then--Cancel or Abort
+					files.delete(path2vpk)
+					return 0
+				end
+				]]
+				return 1
 			end
 
-			if http.download(url, path2vpk).success then
+			local res = http.download(url, path2vpk)
+			if res.headers and res.headers.status_code == 200 then
 				os.delay(500)
 				if files.exists(path2vpk) then
 					files.mkdir("ux0:/data/1luapkg")
@@ -67,7 +74,10 @@ function screen.flip()
 					files.delete("ux0:/data/1luapkg")
 					game.launch(string.format("ONEUPDATE&%s&%s&%s&%s", os.titleid(), path2vpk, files.cdir().."/lang/", __LANG)) -- Goto installer extern!
 				end
+			else
+				os.message(LANGUAGE["UPDATER_ERROR"])
 			end
+			files.delete(path2vpk)
 			onAppInstall = onAppInstallOld
 			onNetGetFile = onNetGetFileOld
 			buttons.homepopup(1)
